17b3b100762181721415e7d3849d44333369f508
David-Olivier Ribault <david@workfluo.com>
2024-11-10 21:06:44 +0100

    ionic init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'national-representation',
+  webDir: 'www'
+};
+
+export default config;
